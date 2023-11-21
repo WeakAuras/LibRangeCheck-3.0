b3975f44afb9f316f3d772acd29a24e0d5d4b8ce
Péter Radics <mitchnull@gmail.com>
2023-11-21 18:10:47 +0100

    bump minor version

--- LibRangeCheck-3.0/LibRangeCheck-3.0.lua
+++ LibRangeCheck-3.0/LibRangeCheck-3.0.lua
@@ -40,7 +40,7 @@ License: MIT
 -- @class file
 -- @name LibRangeCheck-3.0
 local MAJOR_VERSION = "LibRangeCheck-3.0"
-local MINOR_VERSION = 3
+local MINOR_VERSION = 4
 
 local lib, oldminor = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if not lib then
