376d34a0ec4cb5414ceaa3ac03d1fa2324e44e1e
Continuity <continuity@gmx.net>
2024-08-21 15:57:17 +0200

    Version bump.

--- LibRangeCheck-3.0/LibRangeCheck-3.0.lua
+++ LibRangeCheck-3.0/LibRangeCheck-3.0.lua
@@ -40,7 +40,7 @@ License: MIT
 -- @class file
 -- @name LibRangeCheck-3.0
 local MAJOR_VERSION = "LibRangeCheck-3.0"
-local MINOR_VERSION = 24
+local MINOR_VERSION = 25
 
 ---@class lib
 local lib, oldminor = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
